616c89df2f9af2397313f7fe213b70197a42e238
adarshkumarhembram <hembramadarshkumar@gmail.com>
2025-02-17 12:58:49 +0530

    function basics part

---
+++ 02_function/oneFunction.js
@@ -0,0 +1,48 @@
+// console.log('hello Adarsh');
+// console.log('hello Abhay');
+// console.log('hello Guddu');
+// console.log('hello world');
+
+// function sayhi(){
+//     console.log('Hello all');
+//     console.log("it's been nice meeting you all");
+    
+    
+// }
+// sayhi()
+
+//DRY: do not repeat yourself
+
+// function sayhi(name){
+//     console.log(`Hello, ${name}`);
+    
+// }
+// sayhi("Mithun")
+// sayhi("prabir")
+
+// function sayhi(name = 'user'){
+//     console.log(`hello, ${name}` );
+    
+// }
+// sayhi()
+
+// Parameter: Accept; Argument: Pass
+
+// function addNumbers(){
+//     console.log(arguments);    
+// }
+
+// addNumbers(1,2)
+// addNumbers(1,2,3)
+
+function addNumbers(){
+    let sum = 0;
+    for(let i=0; i< arguments.length; i++){
+        sum += arguments[i]
+    }
+    return sum;
+    
+}
+
+const result = addNumbers(1,2)
+console.log(result);
