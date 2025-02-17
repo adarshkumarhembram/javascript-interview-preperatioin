4f5bf8f7c8b42e646c6ad45ae94b9970814fddbe
adarshkumarhembram <hembramadarshkumar@gmail.com>
2025-02-17 16:21:19 +0530

    more about function

--- 02_function/thirdFunction.js
+++ 02_function/thirdFunction.js
@@ -31,8 +31,56 @@
 
 // console.log(sumOFTwoNumbers(2,3));
 
+// function printResult(result){
+//     console.log(`the result is ${result}`);
+    
+// }
+
+// function add(a,b) {return a + b};
+// function subtract(a,b) {return a-b}
+// function multiply(a,b) {return a*b}
+
+// function calculate(operation , a,b, callback){
+//     let result;
+
+//     if(operation == 'abd'){
+//         result = add(a,b)
+//     }else if(operation == 'sub'){   
+//         result = subtract(a,b)
+//     }else if(operation == 'mul'){
+//         result = multiply(a,b)
+//     }else{
+//         console.log("Invalid operation");
+//         return
+        
+//     }
+//     callback(result)
+// }
+
+// calculate('abd', 2, 3, printResult)
+
+// function calculate (a,b ){
+//     const result = a+b;
+//     return function(){
+//         console.log(`The output is ${result}`);        
+//     }
+// }
+
+// calculate(2,3)()
+
+// function addThreeNumbers(a, b, c){
+//     return a+b+c
+// }
+
+// console.log(addThreeNumbers(1,2,3));
 
 
+// function addThreeNumbers(a){
+//     return function(b){
+//         return function(c){
+//             return a+ b + c
+//         }
+//     }
+// }
 
-
-
+// console.log(addThreeNumbers(1)(2)(3));
---
+++ 03_funcdamental/Scope.js
@@ -0,0 +1,17 @@
+//  Global scope
+const a = 'Mithun'
+console.log(a);
+
+//  function scope
+
+function sayhi(){
+    console.log(a);
+    
+}
+sayhi()
+// Block Scope
+{
+    console.log(a);    
+}
+
+
